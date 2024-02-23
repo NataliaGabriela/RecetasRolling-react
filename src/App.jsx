6b45c46e9749a9d20d6ff05052d9950386c14dcb
Natalia Lopez <natigab.24@gmail.com>
2024-02-23 11:18:45 -0300

    navBar y footer completos

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,17 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
-
+import { useState } from "react";
+import "bootstrap/dist/css/bootstrap.min.css";
+import NavBar from "./components/common/NavBar";
+import Footer from "./components/common/Footer";
+import Inicio from "./components/pages/Inicio";
+import "./App.css";
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
     <>
-     
+      <NavBar></NavBar>
+      <Inicio></Inicio>
+      <Footer></Footer>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
