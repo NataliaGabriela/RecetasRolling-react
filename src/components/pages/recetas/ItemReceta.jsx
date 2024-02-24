d8149ec701172d0be463ac07da65c4121cbb349e
Natalia Lopez <natigab.24@gmail.com>
2024-02-24 15:00:40 -0300

    llamamos a la funcion borrarRecetaAPI desde ItemReceta

--- src/components/pages/recetas/ItemReceta.jsx
+++ src/components/pages/recetas/ItemReceta.jsx
@@ -1,5 +1,42 @@
 import { Button } from "react-bootstrap";
+import Swal from "sweetalert2";
+import { borrarRecetaAPI, leerRecetaAPI } from "../../../helpers/queries";
 const ItemReceta = ({receta}) => {
+  const borrarReceta = ()=>{
+    Swal.fire({
+      title: "¿Estas seguro de eliminar esta receta?",
+      text: "No puedes revertir este proceso posteriormente",
+      icon: "warning",
+      showCancelButton: true,
+      confirmButtonColor: "#3085d6",
+      cancelButtonColor: "#d33",
+      confirmButtonText: "Borrar",
+      cancelButtonText: "Cancelar",
+    }).then(async (result) => {
+      if (result.isConfirmed) {
+
+        const respuesta = await borrarRecetaAPI(receta.id)
+        if(respuesta.status === 200){
+          //falta actualizar la tabla
+         const recetasActualizadas = await leerRecetaAPI();
+         setProductos(recetasActualizadas);
+
+          Swal.fire({
+            title: "Receta elminada",
+            text: `La receta de "${receta.nombreReceta}" fue eliminada correctamente`,
+            icon: "success"
+          });
+        }else{
+          Swal.fire({
+            title: "Ocurrio un error",
+            text: `La receta de "${receta.nombreReceta}" no fue eliminada, intente realizar esta operación en unos minutos`,
+            icon: "error"
+          });
+        }
+
+      }
+    });
+  }
   return (
     <tr>
       <td className="text-center">{receta.id}</td>
@@ -17,7 +54,7 @@ const ItemReceta = ({receta}) => {
         >
            <i className="bi bi-pencil-square"></i>
         </Button>
-        <Button className="btnBorrar" >
+        <Button className="btnBorrar" onClick={borrarReceta}>
         <i className="bi bi-trash"></i>
         </Button>
       </td>
