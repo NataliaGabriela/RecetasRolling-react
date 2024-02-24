2869dafb2bdd133a949192788bb5cea841af0c25
Natalia Lopez <natigab.24@gmail.com>
2024-02-24 12:15:46 -0300

    creamos la funcion para crear lareceta en el archivo queries

--- src/helpers/queries.js
+++ src/helpers/queries.js
@@ -11,3 +11,18 @@ export const leerRecetasAPI = async()=>{
         console.log(error)
     }
 }
+export const crearProductoAPI = async(productoNuevo)=>{
+    try {
+        const respuesta = await fetch(URI_PRODUCTOS, {
+            method: "POST",
+            headers:{
+                "Content-Type":"application/json"
+            },
+            body: JSON.stringify(productoNuevo)
+        })
+        console.log(respuesta);
+        return respuesta
+    } catch (error) {
+        console.log(error)
+    }
+}
